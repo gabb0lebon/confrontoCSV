4c01c6eedc866f375cd1d6956586cc2cf1e59e62
gabb0lebon <gabrielbigl03@gmail.com>
2025-03-31 15:27:30 +0200

    Add files via upload

--- static/js/moovment.js
+++ static/js/moovment.js
@@ -1,1 +1,82 @@
+window.addEventListener('popstate', function() {
+    // Forza il ricaricamento della pagina quando l'utente usa il tasto indietro
+    window.location.reload();
+  });
+
+  // Verifica se la pagina è stata caricata dallo storico del browser (tasto indietro)
+  if (performance.getEntriesByType('navigation')[0].type === 'back_forward') {
+    window.location.reload();  // Ricarica la pagina se è stata caricata dal tasto indietro
+  }
+  
+function mostraTextbox1() {
+    document.getElementById("textbox1").style.display = "block"
+    document.getElementById("textbox2").style.display = "none"
+    document.getElementById("textbox3").style.display = "none"
+    document.getElementById("file2").style.visibility = "visible"
+}
+
+function mostraTextbox2() {
+    document.getElementById("textbox1").style.display = "none"
+    document.getElementById("textbox2").style.display = "block"
+    document.getElementById("textbox3").style.display = "none"
+    document.getElementById("file2").style.visibility = "hidden"
+}
+
+function mostraTextbox3() {
+    document.getElementById("textbox1").style.display = "none"
+    document.getElementById("textbox2").style.display = "none"
+    document.getElementById("textbox3").style.display = "block"
+    document.getElementById("file2").style.visibility = "visible"
+}
+
+function refresh() {
+    document.getElementById("textbox1").style.display = "none"
+    document.getElementById("textbox2").style.display = "none"
+    document.getElementById("textbox3").style.display = "none"
+    document.getElementById("file2").style.visibility = "visible"
+
+    var colonne = document.querySelectorAll('.colonnaUP');
+    colonne.forEach(function(colonna) {
+        colonna.style.display = 'block';
+    });
+    var tipologie = document.querySelectorAll('.tipologiaUP');
+    tipologie.forEach(function(tipologia) {
+        tipologia.style.display = 'none';
+    });
+
+    document.getElementById("colonnaDOWN").style.display = "block"
+    document.getElementById("tipologiaDOWN").style.display = "none"
+
+}
+
+function checkboxUP() {
+    const upTipologia = document.querySelectorAll(".tipologiaUP");
+    const upElements = document.querySelectorAll(".colonnaUP");
+
+    upTipologia.forEach(function(element) {
+        if (element.style.display === 'block') {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    });
+
+    upElements.forEach(function(element2) {
+        if (element2.style.display === 'none') {
+            element2.style.display = "block";
+        } else {
+            element2.style.display = "none";
+        }
+    });
+}
+
+function checkboxDOWN() {
+    if (document.getElementById("tipologiaDOWN").style.display == 'block'){
+        document.getElementById("tipologiaDOWN").style.display = "none"
+        document.getElementById("colonnaDOWN").style.display = "block"
+    }else{
+        document.getElementById("tipologiaDOWN").style.display = "block"
+        document.getElementById("colonnaDOWN").style.display = "none"
+    }
+}
 
